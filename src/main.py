bc47d68178458fb144de68a0aba74708b323bd0b
Evgeny Safonov <jeka-luda@yandex.ru>
2023-12-22 19:15:15 +0300

    verification of receipt of vacancies by api

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.job_board import SuperJob, HH
+
+
+if __name__ == '__main__':
+    sj = SuperJob()
+    print(sj.get_vacancies("PHP"))
+
+    # hh = HH()
+    # print(hh.get_vacancies("Python"))
